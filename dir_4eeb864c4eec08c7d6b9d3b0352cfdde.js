1d43c7f640f3da9f0a6eb9d245369923d1bc74ae
fjtrujy <fjtrujy@users.noreply.github.com>
2024-02-06 10:16:06 +0000

    deploy: baefabc9751d226301daffcf54cd28dec0ccb0f1

---
+++ dir_4eeb864c4eec08c7d6b9d3b0352cfdde.js
@@ -0,0 +1,8 @@
+var dir_4eeb864c4eec08c7d6b9d3b0352cfdde =
+[
+    [ "adpenc", "dir_7ae3e066c48cbf1e456bf7b5af53fbfa.html", "dir_7ae3e066c48cbf1e456bf7b5af53fbfa" ],
+    [ "bin2c", "dir_4aa7630bf94b1e948c1a72cd18b9557b.html", "dir_4aa7630bf94b1e948c1a72cd18b9557b" ],
+    [ "gensymtab", "dir_2feef82756d5481f2e3fcf626f1975a7.html", "dir_2feef82756d5481f2e3fcf626f1975a7" ],
+    [ "ps2-irxgen", "dir_92da0d7cbf784bf8c518f9b82538e0f7.html", "dir_92da0d7cbf784bf8c518f9b82538e0f7" ],
+    [ "ps2adpcm", "dir_47818b46b1cf08114dcb647bd070ea80.html", "dir_47818b46b1cf08114dcb647bd070ea80" ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,84 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"audsrv__rpc_8c.html#aef785527990ecea5ad585ea74f420bd9",
+"dir_39d17f469530f79363f6574f3e93b68d.html",
+"dir_cf43a48f832f5c6078b2594de76b351f.html",
+"draw__tests_8h.html#ae3c0c93db565017ea52506622120455f",
+"ee__regs_8h.html#a7998a8aa020c8052a986043a832d17d1",
+"errno_8h.html#ae7df13cc989ed4b6921455a6eff3c249",
+"gif__tags_8h.html#a0e14be95297857642144fcbc324df82e",
+"group__iopmgr.html#gae6d569ac31f371ad395a463fcc0c29c4",
+"gs__gp_8h.html#a8995687da186e66faf4a54aac3b137db",
+"intrman_8h.html#ae9e5efbc2fcad5254df314e8549da246a60195b54805aed254106bc12a4296795",
+"iop_2sound_2audsrv_2src_2adpcm_8c.html#a4c491eaacfe536a7a728c24fb311ab0b",
+"kernel_8h.html#structt__ee__sema",
+"libgs_8h.html#a05e5d4c54e14f90204552efea1cba660",
+"libgs_8h.html#ad43831747c5c99e8d9b3fb395cbcd907",
+"libkbd_8h.html#aa503ca294e126f3e61d85353ba10d97f",
+"libpad_8c_source.html",
+"ncmd_8c.html#a64674d8db70979e561dc15f8088efc12",
+"ps2atad_8c.html",
+"ps2lib__err_8h.html#af71ed046c6b73963069b7eb55da089f6a8818e5d77a84fee4cd87da63f88d39d7",
+"sior__rpc_8h.html",
+"sysclib_8h_source.html",
+"usbd_8h.html#acd6589c77c48046841ecaf23c3ba1ac4"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
